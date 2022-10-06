39a8329b08748d812b7b06d53cbe708d9fa75442
Arik Chakma <arikchangma@gmail.com>
2022-10-06 23:30:32 +0600

    feat: tailwind breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,44 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: ['Silkscreen', 'cursive'],
+        mono: ['IBM Plex Mono', 'monospace'],
+      },
+    },
+    // Breakpoints
+    screens: {
+      '-4xl': { max: '1920px' },
+      // => @media (max-width: 1535px) { ... }
+
+      '-3xl': { max: '1750px' },
+      // => @media (max-width: 1535px) { ... }
+
+      '-2xl': { max: '1512px' },
+      // => @media (max-width: 1512px) { ... }
+
+      '-1xl': { max: '1440px' },
+      // => @media (max-width: 1440px) { ... }
+
+      '-xl': { max: '1279px' },
+      // => @media (max-width: 1279px) { ... }
+
+      '-lg': { max: '1024px' },
+      // => @media (max-width: 1023px) { ... }
+
+      '-2md': { max: '834px' },
+      // => @media (max-width: 834px) { ... }
+
+      '-md': { max: '767px' },
+      // => @media (max-width: 767px) { ... }
+
+      '-sm': { max: '639px' },
+      // => @media (max-width: 639px) { ... }
+
+      '-xs': { max: '428px' }
+      // => @media (max-width: 639px) { ... }
+    }
   },
   plugins: [],
-}
+};
