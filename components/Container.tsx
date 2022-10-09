dbd7a654641e208487a6c80d23d5076f70cc0a04
Arik Chakma <arikchangma@gmail.com>
2022-10-09 20:27:58 +0600

    chore: banner added

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 export default function Container({
   children,
@@ -9,6 +10,7 @@ export default function Container({
   title?: string;
   description?: string;
 }) {
+  const router = useRouter();
   return (
     <div>
       <Head>
@@ -18,6 +20,36 @@ export default function Container({
           content={
             description || 'Explore your fantastic NFT collection on Ethereum.'
           }
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          property="og:image"
+          content="https://tokens-army.vercel.app/static/banner.png"
+        />
+
+        <meta
+          property="og:url"
+          content={`https://tokens-army.vercel.app${router.asPath}`}
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Tokens.Army" />
+        <meta property="og:description" content={description} />
+        <meta
+          property="og:image"
+          content="https://tokens-army.vercel.app/static/banner.png"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="tokens-army.vercel.app" />
+        <meta
+          property="twitter:url"
+          content={`https://tokens-army.vercel.app${router.asPath}`}
+        />
+        <meta name="twitter:title" content="Tokens.Army" />
+        <meta name="twitter:description" content={description} />
+        <meta
+          name="twitter:image"
+          content="https://tokens-army.vercel.app/static/banner.png"
         />
       </Head>
       {children}
