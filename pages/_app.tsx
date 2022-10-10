0d1be6c7ed84845709c1b4dfdb39a952904a2ae3
Arik Chakma <arikchangma@gmail.com>
2022-10-11 00:37:17 +0600

    feat: added a progressbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import '@fontsource/silkscreen';
-import "@fontsource/ibm-plex-mono"
+import '@fontsource/ibm-plex-mono';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import {
@@ -7,12 +7,20 @@ import {
   QueryClientProvider,
   useQuery,
 } from '@tanstack/react-query';
+import NextNProgress from 'nextjs-progressbar';
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
+      <NextNProgress
+        color="#111827"
+        height={2}
+        options={{
+          showSpinner: false,
+        }}
+      />
       <Component {...pageProps} />
     </QueryClientProvider>
   );
